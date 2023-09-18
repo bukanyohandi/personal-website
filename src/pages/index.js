0e64184bf50f30df8906442dd8a08b271637c640
bukanyohandi <yohandistickpage2@gmail.com>
2023-09-18 15:45:24 +0800

    Set maximum height for home page on mobile devices

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import Layout from "../layouts";
+import styled from "styled-components";
+
+const HeightLimiter = styled.div`
+  height: 100vh;
+`;
 
 export default function HomePage() {
   return (
-    <Layout>
-      <Helmet>
-        <title>Yohandi</title>
-        <meta
-          name="description"
-          content={`
+    <HeightLimiter>
+      <Layout>
+        <Helmet>
+          <title>Yohandi</title>
+          <meta
+            name="description"
+            content={`
             Yohandi. A passionate student with a strong interest in algorithms and data structures.
           `}
-        />
-      </Helmet>
-    </Layout>
+          />
+        </Helmet>
+      </Layout>
+    </HeightLimiter>
   );
 }
