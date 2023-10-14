9dccb7d832998dfc0ddf8e9a3730524016b7315a
bukanyohandi <yohandistickpage2@gmail.com>
2023-10-14 11:01:01 +0800

    Fix pdf embed for mobile devices

--- src/components/PDFEmbed.js
+++ src/components/PDFEmbed.js
@@ -1,14 +1,26 @@
 import React from "react";
+import { isMobile } from "react-device-detect";
 
 const PDFEmbed = ({ src, width = "100%", height = "800px" }) => {
   const containerStyle = {
-    // minWidth: "900px",
     width: width,
     height: height,
-    border: "none",
+    border: "1px solid #ccc", // Adjust as needed
     overflow: "hidden",
-    border: "1px",
   };
+
+  if (isMobile) {
+    return (
+      <div>
+        <iframe src={src} style={containerStyle}></iframe>
+        <p>
+          <a href={src} download>
+            Download PDF
+          </a>
+        </p>
+      </div>
+    );
+  }
 
   return <embed src={src} type="application/pdf" style={containerStyle} />;
 };
