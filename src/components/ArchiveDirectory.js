9886e5098625d012737ccdb36284386c8e7b2339
bukanyohandi <yohandistickpage2@gmail.com>
2024-02-09 03:00:14 +0800

    Add back button and display current directory information

--- src/components/ArchiveDirectory.js
+++ src/components/ArchiveDirectory.js
@@ -45,15 +45,62 @@ const Text = styled.span`
   overflow-wrap: break-word;
 `;
 
+const BackButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30px;
+  height: 30px;
+  margin: 0;
+  padding: 4px;
+  background-color: inherit;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+
+  &:hover {
+    background-color: #c2e7ff;
+  }
+`;
+
+const DirectoryName = styled.div`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 100%;
+  margin-left: 8px;
+  margin-right: 8px;
+  font-size: 15px;
+  user-select: none;
+  flex: 1;
+  font-weight: normal;
+  font-family: "Open Sans", sans-serif;
+`;
+
+const NavigationHeader = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 const ArchiveDirectory = ({ location, onFileSelect }) => {
   const [subfolders, setSubfolders] = useState([]);
   const [files, setFiles] = useState([]);
   const [selectedFile, setSelectedFile] = useState(null); // New state for tracking the selected file
 
+  const handleBack = () => {
+    const pathSegments = currentPath.split("/").filter(Boolean);
+    if (pathSegments.length > 0) {
+      pathSegments.pop();
+      const parentPath = pathSegments.join("/");
+      navigate(`/archive/${parentPath}`);
+    }
+  };
+
   const currentPath = location.pathname
     .replace("/archive/", "")
     .replace(/\/$/, "");
   const paths = currentPath.split("/").filter(Boolean);
+  const currentDirectoryName = paths.length > 0 ? paths[paths.length - 1] : "";
 
   const data = useStaticQuery(graphql`
     query {
@@ -105,6 +152,16 @@ const ArchiveDirectory = ({ location, onFileSelect }) => {
 
   return (
     <div>
+      <NavigationHeader>
+        {paths.length > 0 && (
+          <BackButton onClick={handleBack} aria-label="Go back">
+            <div className="back-icon" />{" "}
+          </BackButton>
+        )}
+        <DirectoryName title={currentDirectoryName}>
+          {currentDirectoryName}
+        </DirectoryName>
+      </NavigationHeader>
       <DirectoryList>
         {subfolders.map((folderName) => (
           <DirectoryItem
