7d17b2c534062d06f4deb169ffe55aceb3092aab
bukanyohandi <yohandistickpage2@gmail.com>
2024-02-25 23:57:10 +0800

    Adjust PDFEmbed's height

--- src/components/PDFEmbed.js
+++ src/components/PDFEmbed.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { isMobile } from "react-device-detect";
 
-const PDFEmbed = ({ src, width = "100%", height = "800px" }) => {
+const PDFEmbed = ({ src, width = "100%", height = "100%" }) => {
   const containerStyle = {
     width: width,
     height: height,
+    minHeight: "400px",
     border: "1px solid #ccc", // Adjust as needed
     overflow: "hidden",
   };
